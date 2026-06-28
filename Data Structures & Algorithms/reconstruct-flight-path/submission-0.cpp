ee51346344aa2e0da24a49476e5d460e900eb41a
Ryan Howe <ryanhowe07@hotmail.com>
2026-06-28 17:00:37 -0400

    Add: reconstruct-flight-path - submission-0

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void dfs(unordered_map<string, multiset<string>>& adj, const string& curr, 
+             vector<string>& order) {
+        if (adj[curr].size() == 0) {
+            order.push_back(curr);
+            return;
+        }
+        while (!adj[curr].empty()) {
+            // pull the first thing from the list
+            string nxt = *adj[curr].begin();
+            adj[curr].erase(adj[curr].begin());
+            dfs(adj, nxt, order);
+        }
+        order.push_back(curr);
+    }
+
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        unordered_map<string, multiset<string>> adj;
+        for (const auto& ticket : tickets) {
+            adj[ticket[0]].insert(ticket[1]);
+        }
+        vector<string> order;
+        dfs(adj, "JFK", order);
+        reverse(order.begin(), order.end());
+        return order;
+    }
+};
