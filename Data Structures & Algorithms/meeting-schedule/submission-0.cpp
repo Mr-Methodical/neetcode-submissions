722fc9429788d5c0a84e7251876b76b743fa3417
Ryan Howe <ryanhowe07@hotmail.com>
2026-07-01 07:32:59 -0400

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), 
+        [](const Interval v1, const Interval v2) { return v1.start < v2.start; });  
+        int n = intervals.size();
+        for (int i = 0; i < n - 1; ++i) {
+            if (intervals[i].end > intervals[i + 1].start) return false;
+        }
+        return true;
+    }
+};
